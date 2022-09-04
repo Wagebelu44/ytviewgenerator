6da1c67dc96be22bd32a8fa111be2a1a52d80bb2
Faraz Ahmed Abir <62275863+farazabir@users.noreply.github.com>
2022-09-04 16:12:35 +0600

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='footer'>
-     <h3>Made By: <span>Faraz Ahmed Abir</span></h3>
+     <h4>Made with <span>❤️</span> by Faraz Ahmed Abir</h4>
     </div>
   )
 }
